622e5aa6c8a2de10dd5c9639e6ddfe325180e939
hsiuter <32121068+hsiuter@users.noreply.github.com>
2023-09-15 02:33:49 +0800

    Update settings.py

--- vue_django/vue_django/settings.py
+++ vue_django/vue_django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h=@^7)y=92@ey$gcq&q8e#h3-+o(^w-ur0(2^zra40eps!7h%^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
